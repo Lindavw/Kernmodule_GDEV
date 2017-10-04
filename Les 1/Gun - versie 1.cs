7c5c678719dbae7304d2acafd1bdbff9b578db56
Lindavw <31700542+Lindavw@users.noreply.github.com>
2017-10-04 14:37:13 +0200

    Add files via upload

---
+++ Les 1/Gun - versie 1.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public bool isAutomatic;
+	public float damage = 10f;
+	public float range = 100f;
+	public float fireRate = 15f;
+
+	public Camera fpsCam;
+	public ParticleSystem muzzleFlash;
+	public GameObject impactEffect;
+
+	public int maxAmmo = 10;
+	private int currentAmmo;
+	public float reloadTime = 1f;
+	private bool isReloading = false;
+
+	private float nextTimeToFire = 0f;
+
+	public Text ammoText;
+	public Animator animator;
+
+	public AudioClip shootAudio;
+	public AudioClip reloadAudio;
+
+	void Start(){
+
+		currentAmmo = maxAmmo;
+	}
+
+	void OnEnable(){
+		isReloading = false;
+		animator.SetBool ("Reloading", false);
+	}
+
+	void Update () {
+
+		ammoText.text = currentAmmo.ToString("f0") + ("/") + maxAmmo.ToString("f0");
+
+		if (isReloading)
+			return;
+		
+		if (currentAmmo <= 0 || Input.GetKeyDown("r")) {
+			StartCoroutine(Reload());
+			AudioManager.instance.PlaySound (reloadAudio, transform.position);
+			return;
+		}
+
+		if (isAutomatic == false) {
+			if (Input.GetButtonDown ("Fire1") && Time.time >= nextTimeToFire) {
+				nextTimeToFire = Time.time + 1f / fireRate;
+				Shoot ();
+			}
+		}
+
+		if (isAutomatic == true) {
+			if (Input.GetButton ("Fire1") && Time.time >= nextTimeToFire) {
+				nextTimeToFire = Time.time + 1f / fireRate;
+				Shoot ();
+			}
+		}
+
+	}
+
+	IEnumerator Reload(){
+
+		isReloading = true;
+	
+		animator.SetBool ("Reloading", true);
+
+		yield return new WaitForSeconds (reloadTime - .25f);
+		animator.SetBool ("Reloading", false);
+		yield return new WaitForSeconds (.25f);
+
+		currentAmmo = maxAmmo;
+		isReloading = false;
+	}
+
+	void Shoot(){
+
+		currentAmmo--;
+		
+		muzzleFlash.Play ();
+
+		RaycastHit hit;
+
+		if (Physics.Raycast (fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+		{
+			Enemy enemy = hit.transform.GetComponent<Enemy>();
+			if (enemy != null) 
+			{
+				enemy.TakeDamage (damage);
+			}
+
+			GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation (hit.normal));
+			Destroy (impactGO, 2f);
+		}
+
+		AudioManager.instance.PlaySound (shootAudio, transform.position);
+	}
+}
---
+++ Les 1/PlayerDamage - versie 2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDamage : MonoBehaviour {
+
+	public Text healthPercentage;
+	public Image healthBarP;
+
+	public float startHealthP = 100f;
+	public float playerHealth;
+
+	public float packHealth;
+
+	public PauseMenu pauseMenu;
+
+	private void Start() {
+
+		healthPercentage.text = ("100%");
+		playerHealth = startHealthP;
+	}
+
+	private void Update() {
+
+		healthBarP.fillAmount = playerHealth / startHealthP;
+		healthPercentage.text = playerHealth.ToString("f0") + ("%");
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		
+		if (playerHealth <= startHealthP - packHealth) {
+			if (other.tag == "Health") {
+				playerHealth += packHealth;
+				Destroy (other.gameObject);
+				AudioManager.instance.PlaySound ("HealthPack", transform.position);
+			}
+		}
+	}
+
+	public void TakeDamage(float damount) {
+
+		AudioManager.instance.PlaySound ("PlayerDamaged", transform.position);
+		playerHealth -= damount;
+
+		if (playerHealth <= 0f) {
+			healthPercentage.text = ("0%");
+			playerHealth = 0;
+			Die ();
+		}
+	}
+
+	private void Die() {
+
+		pauseMenu.DeathMenu ();
+	}
+}
